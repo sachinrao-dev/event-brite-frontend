4b41e2594a2705b163371941c92ebba06fbe3f4a
sachinrao2811 <sachinrao9450@gmail.com>
2023-02-02 16:02:04 +0530

    component make

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from "./Components/Constant/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App.js</h1>
+    <div>
+      <Login />
     </div>
   );
 }
