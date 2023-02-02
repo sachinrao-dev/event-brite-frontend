b52a5bb18f7a6261a5096d5a6c64739ed4cc16b7
sachinrao2811 <sachinrao9450@gmail.com>
2023-02-02 16:51:18 +0530

    login layout

--- src/Components/Constant/Login.js
+++ src/Components/Constant/Login.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import "../Style/LoginStyle.css";
 
 function Login() {
+  const [tokenValue, setTokenValue] = useState("");
+  const TokenHandler = () => {
+    console.log(tokenValue);
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="LoginContainer">
+      <input placeholder="Please enter token......" className="TokenInput" onChange={(e) => setTokenValue(e.target.value)} />
+      <button
+        className="submit"
+        type="button"
+        onClick={TokenHandler}
+      >
+        Submit
+      </button>
     </div>
   );
 }
